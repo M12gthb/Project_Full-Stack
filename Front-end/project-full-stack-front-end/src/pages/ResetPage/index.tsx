6069db91dbc7af8a200bf14749a38b3c0c3fad32
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-01-05 07:41:30 -0300

    feat: ResetPage header

--- Front-end/project-full-stack-front-end/src/pages/ResetPage/index.tsx
+++ Front-end/project-full-stack-front-end/src/pages/ResetPage/index.tsx
@@ -1,3 +1,37 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Header } from "../../components/Header";
+import { ModalEditAddress } from "../../components/Modal/ModalEditAddress";
+import { ModalEditUser } from "../../components/Modal/ModalEditUser";
+
 export const ResetPassword = () => {
-  return <></>;
+  const navigate = useNavigate();
+  const [modalEditUserOpen, setmodalEditUserOpen] = useState(false);
+  const [modalEditAddressOpen, setmodalEditAddressOpen] = useState(false);
+
+  const toggleModalEditAddress = () =>
+    setmodalEditAddressOpen(!modalEditAddressOpen);
+
+  const toggleModalEditUser = () => setmodalEditUserOpen(!modalEditUserOpen);
+  useEffect(() => {
+    const token = localStorage.getItem("motors:token");
+    if (token) {
+      navigate("/");
+    }
+  }, []);
+  return (
+    <>
+      <Header
+        user={undefined}
+        toggleModalEditUser={toggleModalEditUser}
+        toggleModalEditAddress={toggleModalEditAddress}
+      />
+      {modalEditAddressOpen ? (
+        <ModalEditAddress toggleModal={toggleModalEditAddress} />
+      ) : null}
+      {modalEditUserOpen ? (
+        <ModalEditUser toggleModal={toggleModalEditUser} />
+      ) : null}
+    </>
+  );
 };
